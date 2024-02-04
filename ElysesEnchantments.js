c39092e07c9534f87ea3a2202ce64eb6eb30ca85
Jess Gallo <54815820+Gallo13@users.noreply.github.com>
2024-02-03 21:11:32 -0500

    Working with arrays
    Push(), Pop(), Splice, Shift(), Unshift()

---
+++ ElysesEnchantments.js
@@ -0,0 +1,25 @@
+/**
+ * Retrieve card from cards array at the 0-based position
+ *
+ * @param {number[]} cards
+ * @param {number} position
+ *
+ * @returns {number} the card
+ */
+export function getItem(cards, position) {
+  return cards[position];
+}
+
+/**
+ * Exchange card with replacementCard at the 0-based position
+ *
+ * @param {number[]} cards
+ * @param {number} position
+ * @param {number} replacementCard
+ *
+ * @returns {number[]} the cards with the change applied
+ */
+export function setItem(cards, position, replacementCard) {
+  cards.splice(position, 1, replacementCard);
+  return cards;
+}
